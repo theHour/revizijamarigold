68807bd8e01dbdb8e166ad3dd3da32744ed38687
Dusan Djukic <dusan.djukic@htecgroup.com>
2024-09-18 17:49:03 +0200

    fix: remove country

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -62,7 +62,7 @@ export default function LandingPage() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
-                  <span className="text-gray-600">Aleja Svetog Save broj 59, 78000, BANJA LUKA, Bosna</span>
+                  <span className="text-gray-600">Aleja Svetog Save broj 59, 78000, BANJA LUKA</span>
                 </div>
               </div>
             </div>
